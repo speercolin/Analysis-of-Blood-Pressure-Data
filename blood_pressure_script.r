373012fe4d5e941f50c5d94a8d004977549b99db
Colin <58573232+speercolin@users.noreply.github.com>
2023-03-01 00:03:29 -0500

    Update blood_pressure_script.r

--- blood_pressure_script.r
+++ blood_pressure_script.r
@@ -9,20 +9,19 @@ diastolic_data <- blood_pressure_data$Mean.diastolic.blood.pressure..mmHg.
 # Pulling alcohol total per capita consumption in liters into separate variable.
 total_alcohol_data <- alcohol_data$Alcohol.total.per.capita..15...consumption.in.liters..numeric.
 
-# Pearson correlation test between systolic BP and alcohol consumption.
-cor.test(systolic_data, total_alcohol_data, method = c("pearson"))
-
 # Creating data frame to be able to create a graph out of our data.
 graph_systolic_data <- data.frame(systolic_data, total_alcohol_data)
 
-# Loading the ggplot2 library for use.
+# Loading ggplot2 library.
 library(ggplot2)
 
-# Utilizing ggplot2 to create a scatter plot comparing systolic BP to alcohol consumption.
+# Utilizing ggplot to create a scatter plot comparing systolic BP to alcohol consumption.
 ggplot(graph_systolic_data, aes(systolic_data, total_alcohol_data)) +
 	geom_point(size = 4, color = "purple") +
 	geom_smooth(method = "lm", se = FALSE, color = "red") +
 	labs(title = "Scatter Plot", systolic_data = "X Values", total_alcohol_data = "Y Values") +
 	theme_classic(base_size = 16) +
 	theme(plot.title = element_text(hjust = 0.5))
-	
+
+# Pearson correlation test between systolic BP and alcohol consumption.
+cor.test(systolic_data, total_alcohol_data, method = c("pearson"))
